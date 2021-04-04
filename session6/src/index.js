43ed2e9d1f86ea372b341372db8d3c6b5ffcbb8f
marwa Radwan <marwaradwan872@hotmail.com>
2021-04-04 11:00:18 +0200

    s6

--- session6/src/index.js
+++ session6/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
-
+const fs = require('fs')
 const app = express()
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../design/views')
@@ -12,5 +12,18 @@ app.set('views',viewsDir)
 hbs.registerPartials(layouts)
 app.use(express.static(publicDir))
 //routes
+// app.get('/new', (req,res)=>{
+// fs.writeFileSync('src/data.json', '[]')
+// res.send('done')
+// })
+
+app.get('', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/add', (req, res)=>{
+    console.log(req.query)
+    res.render('add')
+})
 
 app.listen(3000)
